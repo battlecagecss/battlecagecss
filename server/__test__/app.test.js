d21561921bd45f69f8420030678869dd5a7179f2
Cameron Fitz <hellocameronfitz@gmail.com>
2020-08-11 10:49:06 -0400

    testing works!

---
+++ server/__test__/app.test.js
@@ -0,0 +1,5 @@
+describe('sanity test', () => {
+	it('true is true', () => {
+		expect(true).toBeTruthy();
+	});
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,28 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack');
+const path = require('path');
 
-const entry = ["./client/index.js"];
+const entry = [ './client/index.js' ];
 
 const output = {
-  path: path.resolve(__dirname, "dist"),
-  publicPath: "/dist/",
-  filename: "bundle.js",
+	path: path.resolve(__dirname, 'dist'),
+	publicPath: '/dist/',
+	filename: 'bundle.js'
 };
 
 module.exports = {
-  entry,
-  output,
-  devtool: "eval-source-map",
-  mode: 'development',
-  module: {
-    loaders: [
-      {
-        test: /.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          presets: ["es2015", "react", "stage-2"],
-        },
-      },
-    ],
-  },
+	entry,
+	output,
+	devtool: 'eval-source-map',
+	// mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	}
 };
