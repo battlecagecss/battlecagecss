6c4690ad69a3b0ebaad4d7bcd6a74123e024500e
Cameron Fitz <hellocameronfitz@gmail.com>
2020-08-11 12:39:04 -0400

    Established rooms

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # battlecagecss
 # battlecagecss
+[![Build Status](https://travis-ci.com/battlecagecss/battlecagecss.svg?branch=master)](https://travis-ci.com/battlecagecss/battlecagecss)
+
+
+Battle your friends in a one-on-one CSS domination event.  
+
+Change button colors!
+
+Other things, I assume!
+
--- server/__test__/server.test.js
+++ server/__test__/server.test.js
@@ -11,3 +11,19 @@ describe('GET /', () => {
 		expect(true).toBeTruthy();
 	});
 });
+
+//- /:room (React router, if used)
+// Goes directly to a socket room, but serves the static files also
+//- /:room/site
+//  Serves the current saved version of the changing site
+
+describe('GET /:room', () => {
+	let room = 123;
+	it('should serve static files', () => {
+		return supertest(http).get(`/${room}`).expect(200).expect('Content-Type', 'text/html; charset=UTF-8');
+	});
+
+	it('should direct to a socket room', () => {});
+});
+
+xdescribe('GET /:room/site', () => {});
