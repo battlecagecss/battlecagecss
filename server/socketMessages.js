3cd946b78edacd0de30f20e6444f8222246cf7f7
cupofjoy <eelantung@gmail.com>
2020-08-12 15:29:15 -0400

    addign more tests

--- server/socketMessages.js
+++ server/socketMessages.js
@@ -4,166 +4,183 @@ const fetch = require('node-fetch');
 const rooms = {};
 
 class Player {
-	constructor(socket = '', name = '', pictureURL = '') {
-		this.socket = socket;
-		this.name = name;
-		this.wins = 0;
-		this.pictureURL = pictureURL;
-		this.vote = ''; // should hold socketID of the player this player has voted FOR
-	}
-	incrementWin() {
-		this.wins++;
-	}
+  constructor(socket = '', name = '', pictureURL = '') {
+    this.socket = socket;
+    this.name = name;
+    this.wins = 0;
+    this.pictureURL = pictureURL;
+    this.vote = ''; // should hold socketID of the player this player has voted FOR
+  }
+  incrementWin() {
+    this.wins++;
+  }
 }
 
 class DOMNode {
-	constructor(nodeID = '', attribute = '', value = '') {
-		this.nodeID = nodeID;
-		this.attribute = attribute;
-		this.value = value;
-	}
+  constructor(nodeID = '', attribute = '', value = '') {
+    this.nodeID = nodeID;
+    this.attribute = attribute;
+    this.value = value;
+  }
 }
 
 class Room {
-	constructor(io, room) {
-		this.io = io;
-		this.room = room;
-		this.timer = new Timer(io, room);
-		this.html = this.getHTML();
-		this.node = null;
-		this.cssAttr = null;
-		this.Player1 = {
-			votes: 0,
-			changes: new DOMNode(),
-			player: new Player()
-		};
-		this.Player2 = {
-			votes: 0,
-			changes: new DOMNode(),
-			player: new Player()
-		};
-		this.playerList = [];
-	}
+  constructor(io, room) {
+    this.io = io;
+    this.room = room;
+    this.timer = new Timer(io, room);
+    this.html = this.getHTML();
+    this.node = null;
+    this.cssAttr = null;
+    this.Player1 = {
+      votes: 0,
+      changes: new DOMNode(),
+      player: new Player()
+    };
+    this.Player2 = {
+      votes: 0,
+      changes: new DOMNode(),
+      player: new Player()
+    };
+    this.playerList = [];
+  }
 
-	getPlayers(socket) {
-		return socket.emit('returnPlayerList', this.playerList);
-	}
+  getPlayers(socket) {
+    return socket.emit('returnPlayerList', this.playerList);
+  }
 
-	addPlayer(socket, player) {
-		const newPlayer = new Player(socket, player.name, player.pictureURL);
-		socket.join(this.room);
-		this.playerList.push(newPlayer);
-		this.updatePlayerlist();
-	}
+  addPlayer(socket, player) {
+    const newPlayer = new Player(socket, player.name, player.pictureURL);
+    socket.join(this.room);
+    this.playerList.push(newPlayer);
+    this.updatePlayerlist();
+  }
 
-	removePlayer(socketID) {
-		this.playerList = this.playerList.filter((player) => player.socket.id !== socketID);
-		this.updatePlayerlist();
-	}
+  removePlayer(socketID) {
+    this.playerList = this.playerList.filter((player) => player.socket.id !== socketID);
+    this.updatePlayerlist();
+  }
 
-	updatePlayerlist() {
-		this.io.to(this.room).binary(false).emit('updatePlayerList', this.playerList.map((player) => player.name));
-	}
+  updatePlayerlist() {
+    this.io.to(this.room).binary(false).emit('updatePlayerList', this.playerList.map((player) => player.name));
+  }
 
-	submitVote(socketID, vote) {
-		//find the player who matches the ID
-		const votingPlayer = this.playerList.filter((player) => player.socket.id === socketID);
-		// change their vote to the socketID they voted FOR
-		votingPlayer.vote = vote;
-		this.tallyVotes();
-	}
+  submitVote(socketID, vote) {
+    //find the player who matches the ID
+    const votingPlayer = this.playerList.filter((player) => player.socket.id === socketID);
+    // change their vote to the socketID they voted FOR
+    votingPlayer.vote = vote;
+    this.tallyVotes();
+  }
 
-	tallyVotes() {
-		this.Player1.votes = 0;
-		this.Player2.votes = 0;
-		// go through everyone in the playerList,
-		this.playerList.forEach((player) => {
-			// check who they voted for
-			if (player.vote === this.Player1.player.socket.id) this.Player1.votes++;
-			if (player.vote === this.Player2.player.socket.id) this.Player2.votes++;
-		});
-		this.updateVotes();
-	}
+  tallyVotes() {
+    this.Player1.votes = 0;
+    this.Player2.votes = 0;
+    // go through everyone in the playerList,
+    this.playerList.forEach((player) => {
+      // check who they voted for
+      if (player.vote === this.Player1.player.socket.id) this.Player1.votes++;
+      if (player.vote === this.Player2.player.socket.id) this.Player2.votes++;
+    });
+    this.updateVotes();
+  }
 
-	updateVotes() {
-		const Player1 = this.Player1;
-		const Player2 = this.Player2;
-		this.io.to(this.room).emit('updateVotes', { Player1, Player2 });
-	}
+  updateVotes() {
+    const Player1 = this.Player1;
+    const Player2 = this.Player2;
+    this.io.to(this.room).emit('updateVotes', {
+      Player1,
+      Player2
+    });
+  }
 
-	resetVotes() {
-		this.playerList = this.playerList.map((player) => {
-			player.vote = '';
-			return player;
-		});
-		this.updateVotes();
-	}
+  resetVotes() {
+    this.playerList = this.playerList.map((player) => {
+      player.vote = '';
+      return player;
+    });
+    this.updateVotes();
+  }
 
-	submitChange(socket, value) {
-		if (socket.id === this.Player1.player.socket.id) this.Player1.changes.value = value;
-		if (socket.id === this.Player2.player.socket.id) this.Player2.changes.value = value;
-	}
+  submitChange(socket, value) {
+    if (socket.id === this.Player1.player.socket.id) this.Player1.changes.value = value;
+    if (socket.id === this.Player2.player.socket.id) this.Player2.changes.value = value;
+  }
 
-	returnChanges() {
-		const Player1 = this.Player1;
-		const Player2 = this.Player2;
-		this.io.to(this.room).emit('playerCSSChanges', { Player1, Player2 });
-	}
+  returnChanges() {
+    const Player1 = this.Player1;
+    const Player2 = this.Player2;
+    this.io.to(this.room).emit('playerCSSChanges', {
+      Player1,
+      Player2
+    });
+  }
 
-	getHTML() {
-		// fetch call goes here
-		return fetch('https://developer.mozilla.org/en-US/')
-			.then((x) => x.text())
-			.then((string) => string.replace(/\/static/g, 'https://developer.mozilla.org/static'))
-			.then((resp) => {
-				this.updateHtml(resp);
-			});
-	}
+  getHTML() {
+    // fetch call goes here
+    return fetch('https://developer.mozilla.org/en-US/')
+      .then((x) => x.text())
+      .then((string) => string.replace(/\/static/g, 'https://developer.mozilla.org/static'))
+      .then((resp) => {
+        this.updateHtml(resp);
+      });
+  }
 
-	updateHtml(html) {
-		this.html = html;
-		this.sendHtml();
-	}
+  updateHtml(html) {
+    this.html = html;
+    this.sendHtml();
+  }
 
-	sendHtml() {
-		this.io.to(this.room).emit('newPage', { html: this.html });
-	}
+  sendHtml() {
+    this.io.to(this.room).emit('newPage', {
+      html: this.html
+    });
+  }
 }
 
 class Timer {
-	constructor(io, room) {
-		this.io = io;
-		this.room = room;
-		this.time = 0;
-		this.cb = {};
-	}
+  constructor(io, room) {
+    this.io = io;
+    this.room = room;
+    this.time = 0;
+    this.cb = {};
+  }
 
-	updateTimer() {
-		this.io.to(this.room).emit('timer', { timeRemaining: this.time });
-	}
-	setCountdown(time, cb) {
-		this.time = time;
-		this.cb = cb;
-	}
-	runCountdown() {
-		// if there is no time left, emit a time's up signal
-		if (!this.time) return this.timeUp();
-		// otherwise, emit an update timer with the time and set a setTimeout
-		this.updateTimer();
-		this.time -= 1;
-		setTimeout(this.runCountdown, 1000);
-	}
+  updateTimer() {
+    this.io.to(this.room).emit('timer', {
+      timeRemaining: this.time
+    });
+  }
+  setCountdown(time, cb) {
+    this.time = time;
+    this.cb = cb;
+  }
+  runCountdown() {
+    // if there is no time left, emit a time's up signal
+    if (!this.time) return this.timeUp();
+    // otherwise, emit an update timer with the time and set a setTimeout
+    this.updateTimer();
+    this.time -= 1;
+    setTimeout(() => this.runCountdown(), 1000);
+  }
 
-	timeUp() {
-		this.io.to(this.room).emit('timeUp');
-	}
+  timeUp() {
+    this.io.to(this.room).emit('timeUp');
+  }
 }
 
 const createPlayer = (io, socket, roomName, player) => {
-	if (!rooms[roomName]) rooms[roomName] = new Room(io, roomName);
-	rooms[roomName].addPlayer(socket, player);
-	rooms[roomName].sendHtml();
-	return rooms[roomName];
+  if (!rooms[roomName]) rooms[roomName] = new Room(io, roomName);
+  rooms[roomName].addPlayer(socket, player);
+  rooms[roomName].sendHtml();
+  return rooms[roomName];
 };
 
-module.exports = { createPlayer, Player, Room, rooms, Timer, DOMNode };
+module.exports = {
+  createPlayer,
+  Player,
+  Room,
+  rooms,
+  Timer,
+  DOMNode
+};
